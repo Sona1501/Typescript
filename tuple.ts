c0620ac4128d581469598548425c4ff7b0c317ca
Sona1501 <107426175+Sona1501@users.noreply.github.com>
2023-02-07 23:02:43 +0530

    Tuple Using Typescript

---
+++ tuple.ts
@@ -0,0 +1,10 @@
+let role :[string,string,number,number,boolean] =["sonakshi","sona",12,21,true];
+role.push(true);
+role[1]="users";
+role[2]=1501;
+console.log(role);
+
+var a = ["hello", "sonakshi"];
+var b = a[0], c = a[1];
+console.log(b);
+console.log(c);
