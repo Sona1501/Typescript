462d80a0d8f6c3127cfcd73768134326d376c268
Sona1501 <107426175+Sona1501@users.noreply.github.com>
2023-02-07 23:00:08 +0530

    Add files via upload

---
+++ Union.ts
@@ -0,0 +1,13 @@
+function combine(a:number | string, b:number | string)
+{
+    if(typeof a === "number" && typeof b === 'number') {
+        return a+b;
+    }
+    else{
+        return a.toString()  +  b.toString();
+    }
+
+}
+console.log(combine(10, 20));
+console.log(combine('sonakshi',  'raj'));
+
