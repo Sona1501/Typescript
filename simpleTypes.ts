953213661c650133e114229849f4c3e26003a939
Sona1501 <107426175+Sona1501@users.noreply.github.com>
2023-02-07 23:04:15 +0530

    SimpleTypes using Typescript

---
+++ simpleTypes.ts
@@ -0,0 +1,13 @@
+ var firstName = "sonakshi";
+// firstName = 20;
+ //Implicit Assignment
+console.log(firstName);
+
+var firstName1 : string = "sona";
+ // Explicit Assignment
+ console.log(firstName1);
+
+
+
+
+
