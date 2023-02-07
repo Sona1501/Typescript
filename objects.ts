cd72f44fe2c4ad6aad84050f7439be84a14bc0e5
Sona1501 <107426175+Sona1501@users.noreply.github.com>
2023-02-07 23:07:45 +0530

    Add files via upload

---
+++ objects.ts
@@ -0,0 +1,14 @@
+type objType = {name:string,age:number,email:string}
+
+var Person:objType={
+    name:"sonakshi",
+    age:21,
+    email:"abc@test.com"
+};
+var Users:objType={
+    name:"sona",
+    age:20,
+    email:"xyz@test.com"
+};
+
+console.log(Users.name, Users.age, Person.name);
